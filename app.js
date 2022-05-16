777623b4c3a7bc2bbeeea9e35eee67e40e1c45e0
daeunKim <kde.ryan31@gmail.com>
2022-05-16 15:13:00 +0900

    Add folder structure & connect mongodb

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+require("./config/mongo.connection");
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const indexRouter = require('./routes/indexRouter');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.use(function(req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
